26770f2fe46e1aca5420403107d6402b2a02f008
Konnoyuuta <roshell.o2k@gmail.com>
2023-06-17 12:23:23 +0900

    2-1課題提出

--- 2-1/src/study/Main.java
+++ 2-1/src/study/Main.java
@@ -18,16 +18,11 @@ public class Main {
 
 	        //③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
 	        //   要素が奇数の場合には「nは奇数です。」と出力されます。
-	        	for (int i = 11; i < 20;i++) { 
-	        		if(i % 2 == 1) {
-	        			System.out.print(i + "は奇数です。");
-	        		}
-	        		}
-
 	        
-	        }
-	    
-
+	        for(int i = 0; i < arr.length; i++) {
+	            isOdd(arr[i]);}
+	  }
+	            
 	    //② 関数を定義しなさい。
 	    //   関数名：getName
 	    //  修飾子：public
@@ -46,7 +41,8 @@ public class Main {
 	    public static void isOdd(int num) {
 	      if(num % 2 != 0) {
 	        System.out.println(num + "は奇数です。");
+	      
 	        }
 	  }
-
-}
+	  }
+	 
